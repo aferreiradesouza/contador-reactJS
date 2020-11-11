18022aec1db683a0e4858e8c963063213e4b0bed
Arthur Ferreira <aferreiradesouza97@gmail.com>
2020-11-11 12:26:24 -0300

    transofrmei em lista

---
+++ models/list.js
@@ -0,0 +1,10 @@
+export const list = [
+    {id: 1, nome: 'teste1', valor: 20, quantidade: 0,},
+    {id: 2, nome: 'teste2', valor: 30, quantidade: 0,},
+    {id: 3, nome: 'teste3', valor: 40, quantidade: 0,},
+    {id: 4, nome: 'teste4', valor: 50, quantidade: 0,},
+    {id: 5, nome: 'teste5', valor: 60, quantidade: 0,},
+    {id: 6, nome: 'teste6', valor: 70, quantidade: 0,},
+    {id: 7, nome: 'teste7', valor: 80, quantidade: 0,},
+    {id: 8, nome: 'teste8', valor: 90, quantidade: 0,},
+]
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,71 @@
 import { react, useState } from 'react';
+import { list } from '../models/list'
 
 function home() {
-    const [contador, setContador] = useState(1);
-    
-    function adicionarcontador() {
-        setContador(contador + 1)
+    const [contador, setContador] = useState(0);
+
+    function adicionarcontador(value) {
+        setContador(contador + value)
     }
+
+    function removerContador(value) {
+        setContador(contador - value)
+    };
+
+    function removerTudo(value, total) {
+        setContador(contador - (value * total))
+    }
+
     return (
-    <div>
-        <div>{contador}</div>
-        <button onClick={adicionarcontador}>Adicionar</button>
-    </div>
+        <div>
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Valor</th>
+                    <th></th>
+                </tr>
+                {list.map(e => {
+                    const keys = Object.keys(e);
+                    return <tr>
+                        {keys.map(f => {
+                            return <td>{e[f]}</td>
+                        })}
+                        <td>
+                            <button onClick={() => {
+                                e.quantidade += 1;
+                                adicionarcontador(e.valor);
+                            }}>Adicionar</button>
+                        </td>
+                        {
+                            e.quantidade > 0 ?
+                                <td>
+                                    <button onClick={() => {
+                                        e.quantidade -= 1;
+                                        removerContador(e.valor);
+                                    }}>
+                                        Remover
+                                    </button>
+                                </td>
+                                : null
+                        }
+                        {
+                            e.quantidade >= 10 ?
+                                <td>
+                                    <button onClick={() => {
+                                        removerTudo(e.valor, e.quantidade);
+                                        e.quantidade = 0;
+                                    }}>
+                                        Remover tudo
+                                    </button>
+                                </td>
+                                : null
+                        }
+                    </tr>
+                })}
+            </table>
+            Total: {contador}
+        </div>
     )
 }
 
